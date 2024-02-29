afaf525022cd6c9e71ba7130d7224727d0d322e3
Thống Nguyễn <thong0128@gmail.com>
2024-02-29 16:23:00 +0700

    Debug

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -1,2 +1,58 @@
-package com.codegym.controller;public class ProductController {
+package com.codegym.controller;
+
+import com.codegym.model.Product;
+import com.codegym.model.ProductForm;
+import com.codegym.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+    @Value("${file-upload}")
+    private String fileUpload;
+
+    @Autowired
+    private IProductService productService;
+    @GetMapping("")
+    public String index(Model model) {
+        List<Product> products = productService.findAll();
+        model.addAttribute("products", products);
+        return "/index";
+    }
+    @GetMapping("/create")
+    public ModelAndView showCreateForm(){
+        ModelAndView mav = new ModelAndView("/create");
+        mav.addObject("productForm", new ProductForm());
+        return mav;
+    }
+    @PostMapping("/save")
+    public ModelAndView saveProduct(ProductForm productForm) {
+        MultipartFile multipartFile = productForm.getImage();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(productForm.getImage().getBytes(), new File(fileUpload + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Product product = new Product(productForm.getId(), productForm.getName(), productForm.getDescription(), fileName);
+        productService.save(product);
+        ModelAndView mav = new ModelAndView("/create");
+        mav.addObject("productForm", productForm);
+        mav.addObject("message", "Created new product successfully !");
+        return mav;
+    }
 }
